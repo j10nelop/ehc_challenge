4d9cb91b64bc26b5bc755ffc0e2228d2cb574148
j10nelop <152776722+j10nelop@users.noreply.github.com>
2023-12-21 16:47:50 +0700

    Update bai1.php

--- php_lab/lab4/bai1.php
+++ php_lab/lab4/bai1.php
@@ -31,8 +31,8 @@
             margin-top: 20px;
         }
         button {
-  margin-left: -38px;
-}
+            margin-left: -38px;
+        }
     </style>
 
 </head>
@@ -42,7 +42,15 @@
             <h1>Upload-Image</h1>
             <label for="myfile" class="custom-upload">Choose File</label>
             <input type="file" id="myfile" name="file" style="display: none;" accept="image/*">
+            
             <?php
+            $fileInfo = '';
+            $uploadDir = 'uploads/';
+
+            if (!file_exists($uploadDir) && !is_dir($uploadDir)) {
+                mkdir($uploadDir, 0777, true); // Create the "uploads" directory if it doesn't exist
+            }
+
             if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES['file'])) {
                 $fileName = $_FILES['file']['name'];
                 $fileTmpName = $_FILES['file']['tmp_name'];
@@ -51,19 +59,24 @@
                 if ($fileError === 0) {
                     $fileInfo = getimagesize($fileTmpName);
                     if ($fileInfo !== false) {
-                        move_uploaded_file($fileTmpName, $fileName);
-                        echo '<output id="file-name" aria-live="polite">' . $fileName . '</output>';
-                        echo '<br><img src="' . $fileName . '" alt="Uploaded Image" class="uploaded-image">';
+                        // Move the uploaded file to the "uploads" directory with a unique name
+                        move_uploaded_file($fileTmpName, $uploadDir . $fileName);
                     } else {
                         echo 'The uploaded file is not an image.';
                     }
                 }
-            } else {
-                echo '<output id="file-name" aria-live="polite"></output>';
             }
-            ?><br><br>
+
+            // Display the file info or 'No file selected' if no file is chosen
+            if ($fileInfo !== '') {
+                echo '<output id="file-name" aria-live="polite">' . $fileName . '</output>';
+                echo '<br><img src="' . $uploadDir . $fileName . '" alt="Uploaded Image" class="uploaded-image">';
+            } else {
+                echo '<output id="file-name" aria-live="polite">No file selected</output>';
+            }
+            ?>
+            <br><br>
             <button type="submit" name="submit">Submit</button>
-            
         </form>
     </div>
 </body>
